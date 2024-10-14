341c68339c92ce5c2b28324b1435d40d31d940eb
jhonvalladolid <jhonvalladolidoficial@gmail.com>
2024-10-14 10:55:27 -0500

    Segue agregado

---
+++ UsoCoreData/Tarea.swift
@@ -0,0 +1,13 @@
+//
+//  Tarea.swift
+//  UsoCoreData
+//
+//  Created by Mac05 on 14/10/24.
+//
+
+import Foundation
+
+class Tarea {
+    var nombre = ""
+    var importante = false
+}
--- UsoCoreData/ViewController.swift
+++ UsoCoreData/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tareas:[Tarea] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tareas = crearTareas()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tareas.count
+    }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let tarea = tareas[indexPath.row]
+        
+        if tarea.importante {
+            cell.textLabel?.text = "😊 \(tarea.nombre)"
+        } else {
+            cell.textLabel?.text = "😕 \(tarea.nombre)"
+        }
+        
+        return cell
     }
 
-
+    func crearTareas() -> [Tarea] {
+        let tarea1 = Tarea()
+        tarea1.nombre = "Estudiar para el examen"
+        tarea1.importante = false
+        
+        let tarea2 = Tarea()
+        tarea2.nombre = "Hacer los laboratorios"
+        tarea2.importante = true
+        
+        let tarea3 = Tarea()
+        tarea3.nombre = "Estudiar para subsa"
+        tarea3.importante = false
+        
+        return [tarea1, tarea2, tarea3]
+    }
+    
+    @IBAction func agregarTarea(_ sender: Any) {
+        performSegue(withIdentifier: "agregarSegue", sender: nil)
+    }
+    
 }
 
